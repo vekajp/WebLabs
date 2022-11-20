7e02d1e55785401ae4ee957c56589e3c5d3b250e
vekajp <vekajp@gmail.com>
2022-11-20 22:35:12 +0300

    feat: save wishlist to local storage

--- js/create-wishlist.js
+++ js/create-wishlist.js
@@ -11,3 +11,17 @@ function addWishlistItem() {
     })
     list.appendChild(newItem);
 }
+
+function saveWishlist() {
+    let wishlistItems = []
+    let listItems = document.querySelectorAll(".wishlist-creation_form_item-list")[0].children;
+    let wishListName = document.querySelectorAll(".wishlist-creation_form_wishlist-name")[0].value;
+    for (let listItem of listItems) {
+        let name = listItem.children[0].value;
+        let link = listItem.children[1].value;
+        wishlistItems.push([name, link]);
+    }
+
+    localStorage.setItem(wishListName, JSON.stringify(wishlistItems));
+    alert('Вишлист успешно сохранен\nКоличество вишлистов на данный момент: ' + localStorage.length)
+}
