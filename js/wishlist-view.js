73bb8aea268465393cbda3e0c55280d57175f268
vekajp <vekajp@gmail.com>
2022-11-22 23:49:13 +0300

    fix: decode url path variable

--- js/wishlist-view.js
+++ js/wishlist-view.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let wishlistName =  document.URL.split("?")[1].split("&")[0].split("=")[1];
-    showWishlist(wishlistName);
+    generateWishList();
+    showWishlist();
 });
 
-let showWishlist = function(name) {
+let showWishlist = function() {
+    let name =  decodeURI(document.URL).split("?")[1].split("&")[0].split("=")[1];
     document.querySelector(".wishlist_title").innerHTML = name;
     let list = document.querySelectorAll(".wishlist_items-list")[0];
     let items = JSON.parse(localStorage.getItem(name));
-    for (let i = 0; i < items.length; i++) {
-        let itemName = items[i][0];
-        let link = items[i][1];
+    for (let item of items) {
+        let itemName = item[0];
+        let link = item[1];
         let element = makeElement(itemName, link);
         list.appendChild(element);
     }
@@ -24,4 +25,10 @@ let makeElement = function(name, link) {
         element.innerHTML = `<a class="wishlist_item_text" href=${link}>${name}</a>`
     }
     return element;
+}
+
+let generateWishList = function () {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
 }
